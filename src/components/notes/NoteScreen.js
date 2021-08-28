cf023aa02912012b8aab6da02dfc2f1bfbf46c4f
Luis Carrion <lcarrionalvarez@gmail.com>
2021-08-28 18:04:07 -0500

    Cambiando de referencia dinamica entre notas

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
@@ -7,10 +7,28 @@ export const NoteScreen = () => {
 
   // Guardando la nota seleccionada (activa)
   const {active: note} = useSelector(state => state.notes)
-  const [formValues, handleInputChange] = useForm(note);
-
-   
+  /**
+   * Nuestro Use Form, tiene aqui un problema, y es debido a que maneja el estado de una nota
+   * Y cuando nosotros seleccionamos otra nota distinta, esa nota no va a cambiar el estado actual 
+   * de nuestro useForm, debido a que este maneja su propio estado, por ende, debemos usar el reset del
+   * useForm, para darle mas flexibilidad y cambialo cuando cambia la nota, para ello
+   * 1. Usar un useEffect
+   * 2. Mirar la linea 6 y 7 del useForm
+   * 3. Usar el useRef, para generar un observable, que este comparando la nota activa con la que cambio
+   */
+  const [formValues, handleInputChange, reset] = useForm(note);
   const {body, title} = formValues;
+
+  const activeId = useRef(note.id)
+
+  useEffect(() => {
+    
+    if( note.id !== activeId.current ){ // Si cambia el id de la nota, entonces la resetea
+      reset( note )
+      activeId.current = note.id // Si cambia, se establece la nueva nota activa, (esto evita el ciclo infinito)
+    }
+
+  }, [note, reset])
 
   return (
     <div className="notes__main-content-">
