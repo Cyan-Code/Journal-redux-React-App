e5c57a75d10385d7160a7974911ca3c870c794b2
Luis Carrion <lcarrionalvarez@gmail.com>
2021-09-03 21:50:46 -0500

    test from startSaveNote

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,30 +2,17 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'
 import 'firebase/auth'
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCXYn9DLBM2iM5mSTWtQn9IA2J-UdHE22s",
-  authDomain: "react-app-courses-ad4d8.firebaseapp.com",
-  projectId: "react-app-courses-ad4d8",
-  storageBucket: "react-app-courses-ad4d8.appspot.com",
-  messagingSenderId: "560727922473",
-  appId: "1:560727922473:web:aa3a246182ee3cd7ac8938"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyAQrjRa0O4g7OfmkEPYH2jWs6Py_k8SM6c",
-  authDomain: "test-journal-app-a3ed2.firebaseapp.com",
-  projectId: "test-journal-app-a3ed2",
-  storageBucket: "test-journal-app-a3ed2.appspot.com",
-  messagingSenderId: "1031530273819",
-  appId: "1:1031530273819:web:bccb41975a74a2db4e2cbe"
-};
-
-if( process.env.NODE_ENV === 'test' ) {
-  firebase.initializeApp(firebaseConfigTesting);
-} else {
-  firebase.initializeApp(firebaseConfig);
-}
-
+firebase.initializeApp(firebaseConfig);
 
 
 const db = firebase.firestore();
--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -2,28 +2,36 @@
 
 import configureStore from 'redux-mock-store' //ES6 modules
 import thunk from 'redux-thunk'
-import { startNewNote } from '../../actions/notes'
+import { startNewNote, startSaveNote } from '../../actions/notes'
 import { db } from '../../firebase/firebase-config'
 import { types } from '../../types/types'
  
 const middlewares = [thunk] //estamos usando thunk
 const mockStore = configureStore(middlewares)
 
-const store = mockStore({ // aqui se configura el store, en un determinado punto
+const initState = {
+  auth: {
+    uid: 'TESTING'
+  }
+}
+
+let store = mockStore( // aqui se configura el store, en un determinado punto
   /**
    * Es decir, si para nuestra app, tenemos un determinado store, con determinados
    * estados, debemos tenerlos aqui, creando un store sintetico
    */
-  auth: {
-    uid: 'TESTING'
-  }
+  initState
+)
 
-})
 
 describe('Pruebas con las acciones de notes', () => {
 
   afterAll(() => {
     db.terminate()
+  })
+
+  beforeEach( () => {
+    store.clearActions()
   })
   
   test('Debe crear una nueva nota startNewNote', async () => {
@@ -56,6 +64,46 @@ describe('Pruebas con las acciones de notes', () => {
     await db.doc(`TESTING/journal/notes/${docId}`).delete()
 
   })
+
+  test('Deben de cargarse las notas', async () => {
+    await store.dispatch( startNewNote('TESTING') );
+    const actions = store.getActions();
+
+    expect( actions[1] ).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Object)
+    })
+
+    /* const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(String)
+    }
+
+    expect( actions[0].payload[0] ).toMatchObject(expected) */
+
+  })
+
+  test('Debe de actualizar la nota', async () => {
+    
+    const note = {
+      id: '8Ixlrz0xdRQgPENef6n5',
+      title: 'Titulo',
+      body: 'body'
+    }
+
+    await store.dispatch( startSaveNote(note))
+    const actions = store.getActions();
+    
+    expect( actions[0].type ).toBe( types.notesUpdated )
+
+    const doc = await db.doc(`TESTING/journal/notes/${ note.id }`).get();
+
+    expect( doc.data().title ).toBe( note.title )
+
+  })
+  
 
 })
 
